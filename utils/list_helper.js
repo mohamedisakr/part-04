ef3ee06b6206c8b118cc2f9fad5ae22efa010640
mohamedisakr <mohamedisakr@gmail.com>
2020-06-06 19:30:24 +0200

    implementing exercise 4.7 helper functions and unit tests step 5, lesson a part 4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -15,32 +15,42 @@ const totalLikes = (blogs) => {
 
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) {
-    return 0;
+    return {};
   }
 
   const result = blogs.reduce((a, b) =>
     a.likes > b.likes
-      ? {
-          title: a.title,
-          author: a.author,
-          likes: a.likes,
-        }
-      : {
-          title: b.title,
-          author: b.author,
-          likes: b.likes,
-        }
+      ? { title: a.title, author: a.author, likes: a.likes }
+      : { title: b.title, author: b.author, likes: b.likes }
   );
   return result;
 };
 
 //  returns the author who has the largest amount of blogs.
 const mostBlogs = (blogs) => {
-  const result = blogs.reduce((a, b) => {
-    a.blogs > b.logs
+  if (blogs.length === 0) {
+    return {};
+  }
+  const result = blogs.reduce((a, b) =>
+    a.blogs > b.blogs
       ? { author: a.author, blogs: a.blogs }
-      : { author: b.author, blogs: b.blogs };
-  });
+      : { author: b.author, blogs: b.blogs }
+  );
+  return result;
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
+// returns the author, whose blog posts have the largest amount of likes.
+// The return value also contains the total number of likes that the author has received
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+  const result = blogs.reduce((a, b) =>
+    a.likes > b.likes
+      ? { author: a.author, likes: a.likes }
+      : { author: b.author, likes: b.likes }
+  );
+  return result;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes };
