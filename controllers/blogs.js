e86169a588b75eeee2bd72733b23c0c8266083b3
mohamedisakr <mohamedisakr@gmail.com>
2020-06-07 17:27:27 +0200

    refactor the route handler to use the async/await syntax instead of promises

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,24 +1,30 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs.map((blog) => blog.toJSON()));
-    })
-    .catch((error) => next(error));
+blogsRouter.get("/", async (request, response, next) => {
+  const blogs = await Blog.find({});
+  response.json(blogs.map((blog) => blog.toJSON()));
+
+  // Blog.find({})
+  //   .then((blogs) => {
+  //     response.json(blogs.map((blog) => blog.toJSON()));
+  //   })
+  //   .catch((error) => next(error));
 });
 
-blogsRouter.post("/", (request, response, next) => {
+blogsRouter.post("/", async (request, response, next) => {
   const { title, author, url, likes } = request.body;
   const blog = new Blog({ title, author, url, likes });
+  const savedBlog = await blog.save();
+  response.json(savedBlog.toJSON());
+
   // console.log(blog);
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.json(savedBlog.toJSON());
-    })
-    .catch((error) => next(error));
+  // blog
+  //   .save()
+  //   .then((savedBlog) => {
+  //     response.json(savedBlog.toJSON());
+  //   })
+  //   .catch((error) => next(error));
 });
 
 module.exports = blogsRouter;
