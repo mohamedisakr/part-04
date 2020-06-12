55e01dd4de39554798fe8e8cc0565013ae792ef4
mohamedisakr <mohamedisakr@gmail.com>
2020-06-12 12:15:51 +0200

    implementing 4.19 bloglist expansion step 7 lesson d part 4

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFromRequest = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -19,14 +28,22 @@ blogsRouter.get("/:id", async (req, res) => {
 
 blogsRouter.post("/", async (request, response) => {
   const { title, author, url, likes, userId } = request.body;
-  const user = await User.findById(userId);
+  const token = getTokenFromRequest(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
   const blog = new Blog({
     title,
     author,
     url,
     likes: likes || 0,
-    user: user.id,
-    // user: user._id,
+    // user: user.id,
+    user: user._id,
   });
   const savedBlog = await blog.save();
   user.blogs = user.blogs.concat(savedBlog._id);
